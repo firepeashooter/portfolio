be6b24d873af9b58abada1ad2d9bc4b39ee79c5c
Ben Braun <benjamin.braun2003@gmail.com>
2026-05-30 15:02:05 -0400

    Inital Project Setup

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white">
+			<h1 className="text-4xl font-bold tracking-tight text-teal-400">
+				Tailwind v4 is working!
+			</h1>
+		</div>
+	)
+}
+
+export default App
