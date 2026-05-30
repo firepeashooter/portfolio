275dab10d574bd87ffb62f2dd50a97fdb3c9f7ae
Ben Braun <benjamin.braun2003@gmail.com>
2026-05-30 15:54:32 -0500

    Start Project Card Development

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
-import './Components/ProjectCard.jsx'
+import './index.css'
+import ProjectCard from './Components/ProjectCard.jsx'
 
 function App() {
 
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white">
-			<h1 className="text-4xl font-bold tracking-tight text-teal-400">
-				Tailwind v4 is working!
-			</h1>
+			<ProjectCard />
 		</div>
 	)
 }
--- portfolio/src/Components/ProjectCard.jsx
+++ portfolio/src/Components/ProjectCard.jsx
@@ -1,7 +1,15 @@
 
 function ProjectCard() {
 
+	return (
+		<div className="flex flex-col p-15 rounded-xl bg-sky-950 ">
+			<h1>Project Card</h1>
+
+		</div>
+
+
+	)
 
 }
 
-export default ProjectCard();
+export default ProjectCard;
