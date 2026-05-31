ccb35d67aa0536794762a2d9dc38481a20f3c966
Ben Braun <benjamin.braun2003@gmail.com>
2026-05-31 19:21:33 -0400

    Add Basic Mobile Hero Layout

--- portfolio/src/Components/Hero.jsx
+++ portfolio/src/Components/Hero.jsx
@@ -4,17 +4,27 @@ function Hero() {
 
 	return (
 
-		<div className="flex flex-col pt-45">
 
-			<img
-				src={rayquazaImg}
-				alt="RAYQUAZA"
-				className="w-64 h-64 rounded-full"
+		<div>
+			<div className="flex flex-col justify-center items-center pt-20">
+
+				<img
+					src={rayquazaImg}
+					alt="RAYQUAZA"
+					className="w-64 h-64 rounded-full m-10"
+				/>
+
+				<div className="flex flex-col justify-center items-center font-sans">
+
+					<h2 className="text-5xl font-bold m-3">Hi There</h2>
+					<h2 className="text-5xl font-bold m-3">My Name is <span className="text-green-700">Ray</span></h2>
 
 
 
-			/>
+				</div>
 
+
+			</div>
 
 		</div>
 
