8cc7f7dd166bdd2fd08c98539fb1aa72fae559ad
Ben Braun <benjamin.braun2003@gmail.com>
2026-05-31 19:39:20 -0400

    Responsive Hero section

--- portfolio/src/Components/Hero.jsx
+++ portfolio/src/Components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
 
 
 		<div>
-			<div className="flex flex-col justify-center items-center pt-20">
+			<div className="flex flex-col lg:hidden justify-center items-center pt-25">
 
 				<img
 					src={rayquazaImg}
@@ -14,18 +14,31 @@ function Hero() {
 					className="w-64 h-64 rounded-full m-10"
 				/>
 
-				<div className="flex flex-col justify-center items-center font-sans">
+				<div className="flex flex-col justify-center items-center">
 
 					<h2 className="text-5xl font-bold m-3">Hi There</h2>
 					<h2 className="text-5xl font-bold m-3">My Name is <span className="text-green-700">Ray</span></h2>
 
-
-
 				</div>
-
 
 			</div>
 
+			<div className="hidden lg:flex justify-center items-center pt-25">
+
+				<img
+					src={rayquazaImg}
+					alt="RAYQUAZA"
+					className="w-64 h-64 rounded-full m-10"
+				/>
+
+				<div className="flex flex-col justify-center items-center">
+
+					<h2 className="text-5xl font-bold m-3">Hi There</h2>
+					<h2 className="text-5xl font-bold m-3">My Name is <span className="text-green-700">Ray</span></h2>
+
+				</div>
+
+			</div>
 		</div>
 
 
