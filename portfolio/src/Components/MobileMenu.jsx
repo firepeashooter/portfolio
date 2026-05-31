e959e9181726cf9df04d32759496cf8e2c9cc096
Ben Braun <benjamin.braun2003@gmail.com>
2026-05-31 16:37:01 -0500

    Add basic mobile menu

---
+++ portfolio/src/Components/MobileMenu.jsx
@@ -0,0 +1,27 @@
+
+function MobileMenu() {
+
+	return (
+
+		<div className="h-full w-full m-5 border rounded-md border-slate500 p-5">
+
+			<ul className="flex flex-col gap-7">
+				<li className=" p-1 hover:bg-slate-500 rounded-md">About</li>
+				<li className=" p-1 hover:bg-slate-500 rounded-md">Skills</li>
+				<li className=" p-1 hover:bg-slate-500 rounded-md">Projects</li>
+				<li className=" p-1 hover:bg-slate-500 rounded-md">Experience</li>
+
+			</ul>
+
+
+
+
+		</div>
+
+
+
+	)
+
+}
+
+export default MobileMenu;
--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,58 +1,66 @@
+import MobileMenu from "./MobileMenu";
 
 function Navbar() {
 
 	return (
 
-		<nav className="flex items-center h-20 p-10 w-screen fixed bg-slate-950 text-slate-100">
+		<nav className="flex flex-col justify-center items-center min-h-20 p-7 w-screen fixed bg-slate-950 text-slate-100">
 
+			<div className="w-full h-full">
+				{/* THIS IS THE DESKTOP VIEW */}
+				<div className="hidden lg:flex items-center justify-around w-full h-full">
+					<div className=" font-mono text-3xl font-bold">
+						Logo (Desktop)
+					</div>
+					<div className="text-3xl font-bold">
+						<ul className="flex gap-7 font-sans">
 
-			{/* THIS IS THE DESKTOP VIEW */}
-			<div className="hidden lg:flex items-center justify-around w-full h-full">
-				<div className=" font-mono text-3xl font-bold">
-					Logo (Desktop)
+							<li>About </li>
+							<li>Skills </li>
+							<li>Projects </li>
+							<li>Experience </li>
+
+						</ul>
+					</div>
+
+					<div className="text-3xl font-bold">
+						Contact
+					</div>
+
 				</div>
-				<div className="text-3xl font-bold">
-					<ul className="flex gap-7 font-sans">
 
-						<li>About </li>
-						<li>Skills </li>
-						<li>Projects </li>
-						<li>Experience </li>
+				{/* THIS IS THE DEFAULT MOBILE VIEW */}
+				<div className="flex lg:hidden items-center justify-between w-full h-full">
+					<div className=" font-mono text-3xl font-bold">
+						Logo (Mobile View)
+					</div>
 
-					</ul>
-				</div>
+					<button className="flex justify-center items-center rounded-md p-2 h-10 w-10 transition-all duration-200 hover:bg-slate-500">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={2.9}
+							stroke="currentColor"
+							className="h-5 w-5" // Tailwind sets the icon size here
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M2 3.75h23M2 12h23M2 20.25h23"
+							/>
+						</svg>
+					</button>
 
-				<div className="text-3xl font-bold">
-					Contact
 				</div>
 
 			</div>
 
-			{/* THIS IS THE DEFAULT MOBILE VIEW */}
-			<div className="flex lg:hidden items-center justify-around w-full h-full">
-				<div className=" font-mono text-3xl font-bold">
-					Logo (Mobile View)
-				</div>
+			<MobileMenu />
 
-				<button className="flex justify-center items-center border-2 border-slate-500 rounded-md p-2 h-10 w-10">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth={2.9}
-						stroke="currentColor"
-						className="h-5 w-5" // Tailwind sets the icon size here
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M2 3.75h23M2 12h23M2 20.25h23"
-						/>
-					</svg>
-				</button>
-
-			</div>
 		</nav>
+
+
 
 
 
